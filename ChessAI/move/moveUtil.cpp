2724eb58c930f5f6b66c06c857a8bc4947aa3278
Cody Moore <codymmoore97@gmail.com>
2025-01-12 18:32:37 -0600

    Optimize in-check logic

--- ChessAI/move/moveUtil.cpp
+++ ChessAI/move/moveUtil.cpp
@@ -21,19 +21,173 @@ using util::bitboard::BitboardSet;
 
 namespace move
 {
-	// Forward declaration
-	std::vector<Move> getValidMoves(const ChessState& chessState, const Color player, const bool removeChecks, const bool castling);
-
-	bool isUnderAttack(const Color player, const ChessState& chessState, const Position& position)
+	bool isInBounds(const Position& position)
 	{
-		const auto predicate = [&position](const Move& move)
-			{
-				return move.destination == position;
-			};
-		const std::vector<Move> enemyMoves = getValidMoves(chessState, ~player, false, false);
-		const auto it = std::find_if(enemyMoves.begin(), enemyMoves.end(), predicate);
+		return position.x >= 0 && position.x < FILE_COUNT && position.y >= 0 && position.y < RANK_COUNT;
+	}
 
-		return it != enemyMoves.end();
+	bool canBeCapturedByPawn(const ChessState& chessState, const Color player, const Position& position)
+	{
+		if (Color::WHITE == player && position.y == 0 || Color::BLACK == player && position.y == RANK_COUNT - 1)
+		{
+			return false;
+		}
+
+		const Color enemyPlayer = ~player;
+		const Position backward = player == Color::WHITE ? UP : DOWN;
+		const BitboardSet& board = chessState.getBoard();
+
+		Position attackerPos = position + backward + LEFT;
+		if (attackerPos.x > 0 && board.posIsOccupied(attackerPos, enemyPlayer, PieceType::PAWN))
+		{
+			return true;
+		}
+
+		attackerPos = position + backward + RIGHT;
+		return attackerPos.x < RANK_COUNT && board.posIsOccupied(attackerPos, enemyPlayer, PieceType::PAWN);
+	}
+
+	bool canBeCapturedByKnight(const ChessState& chessState, const Color player, const Position& position)
+	{
+		static std::vector<Position> knightDirections = {
+			UP + RIGHT * 2,
+			UP + LEFT * 2,
+			DOWN + RIGHT * 2,
+			DOWN + LEFT * 2,
+			UP * 2 + RIGHT,
+			UP * 2 + LEFT,
+			DOWN * 2 + RIGHT,
+			DOWN * 2 + LEFT
+		};
+		const Color enemyPlayer = ~player;
+		const BitboardSet& board = chessState.getBoard();
+
+		for (const Position& direction : knightDirections)
+		{
+			const Position attackerPos = position + direction;
+			if (isInBounds(attackerPos) && board.posIsOccupied(attackerPos, enemyPlayer, PieceType::KNIGHT))
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	bool canBeCapturedBySlider(const ChessState& chessState, const Color player, const Position& position)
+	{
+		const Color enemyPlayer = ~player;
+		const BitboardSet& board = chessState.getBoard();
+
+		static std::vector<Position> diagonalDirections = {
+			UP + LEFT,
+			UP + RIGHT,
+			DOWN + LEFT,
+			DOWN + RIGHT
+		};
+
+		for (const Position& direction : diagonalDirections)
+		{
+			Position attackerPos = position + direction;
+			while (isInBounds(attackerPos))
+			{
+				if (board.posIsOccupied(attackerPos))
+				{
+					if (board.posIsOccupied(attackerPos, enemyPlayer, PieceType::BISHOP)
+						|| board.posIsOccupied(attackerPos, enemyPlayer, PieceType::QUEEN))
+					{
+						return true;
+					}
+					else
+					{
+						break;
+					}
+				}
+				attackerPos += direction;
+			}
+		}
+
+		static std::vector<Position> straightDirections = {
+			UP,
+			DOWN,
+			LEFT,
+			RIGHT
+		};
+
+		for (const Position& direction : straightDirections)
+		{
+			Position attackerPos = position + direction;
+			while (isInBounds(attackerPos))
+			{
+				if (board.posIsOccupied(attackerPos))
+				{
+					if (board.posIsOccupied(attackerPos, enemyPlayer, PieceType::ROOK)
+						|| board.posIsOccupied(attackerPos, enemyPlayer, PieceType::QUEEN))
+					{
+						return true;
+					}
+					else
+					{
+						break;
+					}
+				}
+				attackerPos += direction;
+			}
+		}
+
+		return false;
+	}
+
+	bool canBeCapturedByKing(const ChessState& chessState, const Color player, const Position& position)
+	{
+		static std::vector<Position> kingDirections = {
+			UP,
+			DOWN,
+			LEFT,
+			RIGHT,
+			UP + LEFT,
+			UP + RIGHT,
+			DOWN + LEFT,
+			DOWN + RIGHT
+		};
+		const Color enemyPlayer = ~player;
+		const BitboardSet& board = chessState.getBoard();
+
+		for (const Position& direction : kingDirections)
+		{
+			const Position attackerPos = position + direction;
+			if (isInBounds(attackerPos) && board.posIsOccupied(attackerPos, enemyPlayer, PieceType::KING))
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	bool canBeCaptured(const Color player, const ChessState& chessState, const Position& position)
+	{
+		if (canBeCapturedByPawn(chessState, player, position))
+		{
+			return true;
+		}
+
+		if (canBeCapturedByKnight(chessState, player, position))
+		{
+			return true;
+		}
+
+		if (canBeCapturedBySlider(chessState, player, position))
+		{
+			return true;
+		}
+
+		if (canBeCapturedByKing(chessState, player, position))
+		{
+			return true;
+		}
+
+		return false;
 	}
 
 	bool canCastle(const Color player, const ChessState& game, const bool kingSide)
@@ -69,8 +223,8 @@ namespace move
 			{
 				// Set safePath to false if any position the king would pass over could be attacked
 				//      (this include the position the king lands on)
-				bool safePath = !(isUnderAttack(player, game, position + direction)
-					|| isUnderAttack(player, game, position + direction * 2));
+				bool safePath = !(canBeCaptured(player, game, position + direction)
+					|| canBeCaptured(player, game, position + direction * 2));
 
 				if (safePath)
 				{
@@ -82,7 +236,7 @@ namespace move
 		return false;
 	}
 
-	std::vector<Move> getValidMoves(const ChessState& chessState, const Color player, const bool removeChecks, const bool castling)
+	std::vector<Move> getValidMoves(const ChessState& chessState, const Color player)
 	{
 		std::vector<Move> result = generatePawnMoves(chessState, player);
 
@@ -102,44 +256,35 @@ namespace move
 		result.insert(result.end(), std::make_move_iterator(kingMoves.begin()), std::make_move_iterator(kingMoves.end()));
 
 		const Position kingStartPosition = ChessState::KING_START_POS[player];
-		if (castling && canCastle(player, chessState, true))
+		if (canCastle(player, chessState, true))
 		{
 			result.emplace_back(kingStartPosition, kingStartPosition + RIGHT * 2);
 		}
 
-		if (castling && canCastle(player, chessState, false))
+		if (canCastle(player, chessState, false))
 		{
 			result.emplace_back(kingStartPosition, kingStartPosition + LEFT * 2);
 		}
 
-		if (removeChecks)
+		std::vector<Move>::iterator move = result.begin();
+		while (move != result.end())
 		{
-			std::vector<Move>::iterator move = result.begin();
+			ChessState gameCopy(chessState);
 
-			while (move != result.end())
+			makeMove(player, *move, gameCopy);
+
+			if (inCheck(player, gameCopy))
 			{
-				ChessState gameCopy(chessState);
-
-				makeMove(player, *move, gameCopy);
-
-				if (inCheck(player, gameCopy))
-				{
-					// Erase current move and set move equal to next move
-					move = result.erase(move);
-				}
-				else
-				{
-					move++;
-				}
+				// Erase current move and set move equal to next move
+				move = result.erase(move);
+			}
+			else
+			{
+				move++;
 			}
 		}
 
 		return result;
-	}
-
-	std::vector<Move> getValidMoves(const ChessState& chessState, const Color player)
-	{
-		return getValidMoves(chessState, player, true, true);
 	}
 
 	bool isValidMove(const Color player, const Position& source, const Position& destination, const ChessState& chessState)
@@ -361,7 +506,7 @@ namespace move
 
 		// Return true if king's position can be attacked by opposing player
 		//      return false otherwise
-		return isUnderAttack(player, chessState, kingPosition);
+		return canBeCaptured(player, chessState, kingPosition);
 	}
 
 	void makeMove(const Color player, const Move move, ChessState& chessState, const PieceType promotion)
